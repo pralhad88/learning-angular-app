784b6a7658c36f82d4ab2996334a9f3ffba1e59f
Pralhad  Bharmal Jadhav <pralhad.j@mantralabs.tech>
2021-07-26 12:57:25 +0530

    added rental component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';  // <<<< import it here
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from "@angular/router";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { HeaderComponent } from './header/header.component';
+import { RentalComponent } from './rental/rental.component';
+
+import { RentalModul } from './rental/rental.module';
+
+const routes: Routes = [
+  {path: "", redirectTo: "/rentals", pathMatch: "full" }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    HeaderComponent
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    // AppRoutingModule,
+    FormsModule,
+    RentalModul,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
